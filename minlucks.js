d8650b9e58d20610afa999b11fafefaad31a88ee
Brad Parbs <brad@bradparbs.com>
2023-05-31 11:10:13 -0500

    Update minlucks.js to 2023.5.31-8

--- minlucks.js
+++ minlucks.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt Minlucks
 // @description Minlucks and power for MouseHunt, from Google Sheets, to power the multiple mini-CRE scripts.
-// @version     2023.5.31-5
+// @version     2023.5.31-8
 // @license     MIT
 // @author      brap
 // @namespace   bradp
